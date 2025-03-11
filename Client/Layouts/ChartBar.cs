701072bced47815aa4743ed0bef9d12e4b5b0508
Raz <moldoveanualexandru@yahoo.com>
2025-03-11 10:18:52 +0100

    Vertical Bars WIP

--- Client/Layouts/ChartBar.cs
+++ Client/Layouts/ChartBar.cs
@@ -5,6 +5,7 @@
         public string Name { get; set; }
         public decimal Percent { get; set; }
         public string Text { get; set; }
+        public int CalculatePer => Percent < 1 ? (int)(Percent * 100) : 100;
 
         public ChartBar(string name, decimal percent, string text)
         {
