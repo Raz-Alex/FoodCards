1c3c9fd44c87f57f82d183484c92c1f61384cb14
Raz <moldoveanualexandru@yahoo.com>
2025-03-11 10:03:20 +0100

    Vertical Bars, WIP

---
+++ Client/Layouts/ChartBar.cs
@@ -0,0 +1,16 @@
+﻿namespace FoodCards.Client.Layouts
+{
+    public class ChartBar
+    {
+        public string Name { get; set; }
+        public decimal Percent { get; set; }
+        public string Text { get; set; }
+
+        public ChartBar(string name, decimal percent, string text)
+        {
+            Name = name;
+            Percent = percent;
+            Text = text;
+        }
+    }
+}
---
+++ Client/Script/DishSetUpData.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodCards.Client.Script
+{
+    public class DishSetUpData
+    {
+        public int DailyKalories { get; set; } = 2000;
+        Dictionary<string, int> Goals { get; set; } = new();
+        Dictionary<string, int> Limits { get; set; } = new();
+    }
+}
